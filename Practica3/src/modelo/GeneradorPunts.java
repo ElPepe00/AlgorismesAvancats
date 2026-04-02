4e20cd6e3dbba2256a8d6c1315497f63318d1fa7
Josep Oliver <josepoliverv@gmail.com>
2026-04-02 11:34:29 +0200

    añadido distribucion exponencial

--- Practica3/src/modelo/GeneradorPunts.java
+++ Practica3/src/modelo/GeneradorPunts.java
@@ -11,14 +11,38 @@ public class GeneradorPunts {
         for (int i = 0; i < n; i++) {
             double x, y;
 
-            if (tipus.equals("gaussiana")) {
-                x = (r.nextGaussian()/6.0) + 0.5;
-                y = (r.nextGaussian()/6.0) + 0.5;
-            } else {
-                x = r.nextDouble();
-                y = r.nextDouble();
+            switch (tipus) {
+                case "Uniforme":
+                    x = r.nextDouble();
+                    y = r.nextDouble();
+                    break;
+                case "Gaussiana":
+                    x = (r.nextGaussian() / 8.0) + 0.5;
+                    y = (r.nextGaussian() / 8.0) + 0.5;
+                    break;
+                case "Exponencial":
+                    x = r.nextExponential() / 5.0;
+                    y = r.nextExponential() / 5.0;
+                    break;
+                default:
+                    throw new AssertionError();
             }
 
+            // OPCIONAL: para asegurar que ningun punto invisible rompa el programa
+            if (x < 0.0) {
+                x = 0.0;
+            }
+            if (x > 1.0) {
+                x = 1.0;
+            }
+            if (y < 0.0) {
+                y = 0.0;
+            }
+            if (y > 1.0) {
+                y = 1.0;
+            }
+            // ------------
+            
             punts.add(new Punt(x, y));
         }
 
