717606c330c298a1e7e51412ee02165e52c6a6dc
Josep Oliver <josepoliverv@gmail.com>
2026-05-03 17:32:26 +0200

    P5 inicial

---
+++ Practica5/src/practica5/Practica5.java
@@ -0,0 +1,19 @@
+
+
+package practica5;
+
+/**
+ *
+ * @author Josep Oliver y Hugo Valls
+ * @date 3 may 2026
+ * @name Practica5
+ */
+public class Practica5 {
+
+    /**
+     * Static Main
+     */
+    public static void main(String[] args) {
+    }
+
+}
