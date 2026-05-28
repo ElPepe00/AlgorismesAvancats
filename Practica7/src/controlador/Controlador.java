34793f0c10200547ee99fabd47cbbf4f7e47346e
Josep Oliver <josepoliverv@gmail.com>
2026-05-28 15:46:03 +0200

    creadas clases p7

---
+++ Practica7/src/controlador/Controlador.java
@@ -0,0 +1,13 @@
+
+
+package controlador;
+
+/**
+ *
+ * @author Josep Oliver y Hugo Valls
+ * @date 28 may 2026
+ * @name Controlador
+ */
+public class Controlador {
+
+}
---
+++ Practica7/src/modelo/Modelo.java
@@ -0,0 +1,13 @@
+
+
+package modelo;
+
+/**
+ *
+ * @author Josep Oliver y Hugo Valls
+ * @date 28 may 2026
+ * @name Modelo
+ */
+public class Modelo {
+
+}
